fcebffc3db6626a173cfb8d2cf8a37c55c14834b
Ozan Müjde <ozanmujde@googlemail.com>
2022-06-12 20:12:55 +0300

    github page test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <main>
         <h1>Hello world</h1>
+        <h2>testing</h2>
       </main>
     </div>
   );
