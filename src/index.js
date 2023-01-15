6951e172a30e64816f3f5b99dbc7d81dd27d4751
Ozan Müjde <ozanmujde@googlemail.com>
2023-01-15 18:14:12 +0300

    last dance

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename="/ozanmujde.github.io">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   // </React.StrictMode>
